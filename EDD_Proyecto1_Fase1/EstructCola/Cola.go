6fba39d8a14819ebb7fa0634d2909ad0616e0866
NatalyDuarte <natalyguzman7777@gmail.com>
2023-02-20 20:09:35 -0600

    Pila

--- EDD_Proyecto1_Fase1/EstructCola/Cola.go
+++ EDD_Proyecto1_Fase1/EstructCola/Cola.go
@@ -1,61 +1,72 @@
 package EstructCola
 
 import (
-	"fmt"
-
 	"Fase1/Usuarios"
+	"fmt"
 )
 
 // Aqui encontramos el nodo
 type Nodo struct {
-	siguiente *Nodo
-	persona   *Usuarios.Usuario
+	Siguiente *Nodo
+	Persona   *Usuarios.Usuario
 }
 
 // Aqui se crea la Cola
 type Col struct {
-	primero  *Nodo
-	ultimo   *Nodo
-	contador int
+	Primero  *Nodo
+	Ultimo   *Nodo
+	Contador int
 }
 
-func NuevoCola() *Col {
-	return &Col{nil, nil, 0}
-}
 func NuevoNodo(usu *Usuarios.Usuario) *Nodo {
 	return &Nodo{nil, usu}
 }
 
-func Insertar(usu *Usuarios.Usuario, colita *Col) int {
+func (colita *Col) Insertar(usu *Usuarios.Usuario) int {
 	//Declarar nuevo nodo
 	var nuevis *Nodo = NuevoNodo(usu)
-	//Verificar si la lista está vacía
-	if colita.primero == nil {
-		colita.primero = nuevis
+	aux := colita.Primero
+	if colita.Primero == nil {
+		colita.Primero = nuevis
 	} else {
-		//Recorrer hasta encontrar el último nodo
-		temp := colita.primero
-		for temp.siguiente != nil {
-			temp = temp.siguiente
+		for aux.Siguiente != nil {
+			aux = aux.Siguiente
 		}
-		//Agregar el nuevo nodo hasta el final
-		temp.siguiente = nuevis
+		aux.Siguiente = nuevis
 	}
-	colita.contador += 1
-	fmt.Println("Usuario inresado correctamente")
-	return colita.contador
+	colita.Contador += 1
+	fmt.Println("Usuario ingresado correctamente")
+	return colita.Contador
 }
 
-// Metodo para imprimir la cla
-func Imprimir(colita *Col) {
-	aux := colita.primero
-	if colita.primero != nil {
-		for aux != nil {
-			fmt.Println(" ", aux.persona.Nombre)
-			aux = aux.siguiente
+// Metodo para eliminar
+func (colita *Col) Eliminar(usu *Usuarios.Usuario) {
+	actual := colita.Primero
+	var anterior *Nodo = nil
+	var encontrado bool = false
+	if colita.Primero != nil {
+		for actual != nil && encontrado != true {
+			if actual.Persona.Nombre == usu.Nombre {
+				fmt.Println(" Nodo con el dato ( ", usu.Nombre, " ) Encontrado")
+				if actual == colita.Primero {
+					colita.Primero = colita.Primero.Siguiente
+				} else if actual == colita.Ultimo {
+					anterior.Siguiente = nil
+					colita.Ultimo = anterior
+				} else {
+					anterior.Siguiente = actual.Siguiente
+				}
+				fmt.Println("Nodo Eliminado")
+				colita.Contador -= 1
+				encontrado = true
+			}
+			anterior = actual
+			actual = actual.Siguiente
+		}
+		if !encontrado {
+			fmt.Println("Nodo No Encontrado")
 		}
 	} else {
 		fmt.Println(" La cola se encuentra Vacia ")
-
 	}
 }
--- EDD_Proyecto1_Fase1/EstructListaDob/ListaDoble.go
+++ EDD_Proyecto1_Fase1/EstructListaDob/ListaDoble.go
@@ -8,41 +8,73 @@ import (
 
 // Aqui encontramos el nodo
 type NodoD struct {
-	siguiente *NodoD
-	atras     *NodoD
-	persona   *Usuarios.Usuario
+	Siguiente *NodoD
+	Atras     *NodoD
+	Persona   *Usuarios.Usuario
 }
 
 // Aqui se crea la Lista Doble
 type ListaDobl struct {
-	primero  *NodoD
-	ultimo   *NodoD
-	contador int
+	Primero  *NodoD
+	Ultimo   *NodoD
+	Contador int
 }
 
-func NuevoListDob() *ListaDobl {
-	return &ListaDobl{nil, nil, 0}
-}
 func NuevoNodoDob(usu *Usuarios.Usuario) *NodoD {
 	return &NodoD{nil, nil, usu}
 }
-func InsertarDob(usu *Usuarios.Usuario, listat *ListaDobl) {
+func (listat *ListaDobl) InsertarDob(usu *Usuarios.Usuario) {
 	//Declarar nuevo nodo
 	var nuevis *NodoD = NuevoNodoDob(usu)
 	//Verificar si la lista está vacía
-	if listat.primero == nil {
-		listat.primero = nuevis
-		listat.primero.siguiente = nil
-		listat.primero.atras = nil
-		listat.ultimo = listat.primero
-		listat.contador += 1
+	if listat.Primero == nil {
+		listat.Primero = nuevis
+		listat.Primero.Siguiente = nil
+		listat.Primero.Atras = nil
+		listat.Ultimo = listat.Primero
+		listat.Contador += 1
 	} else {
-		listat.ultimo.siguiente = nuevis
-		nuevis.siguiente = nil
-		nuevis.atras = listat.ultimo
-		listat.ultimo = nuevis
-		listat.contador += 1
+		listat.Ultimo.Siguiente = nuevis
+		nuevis.Siguiente = nil
+		nuevis.Atras = listat.Ultimo
+		listat.Ultimo = nuevis
+		listat.Contador += 1
 	}
 	fmt.Println("Usuario ingresado correctamente a la lista enlazada doble")
+}
 
+// Metodo para imprimir la lista
+func (listat *ListaDobl) Imprimir() {
+	actual := listat.Primero
+	if listat.Primero != nil {
+		listat.Ordenamiento()
+		for actual != nil {
+			fmt.Println("Nombre: ", actual.Persona.Nombre, " ", actual.Persona.Apellido, " , Carnet: ", actual.Persona.Carnet)
+			fmt.Println("=======================================================")
+			actual = actual.Siguiente
+		}
+	} else {
+		fmt.Println(" La lista doble se encuentra Vacia ")
+	}
+}
+
+// Metodo de ordenamiento de burbujas
+func (listat *ListaDobl) Ordenamiento() {
+	comprobar := listat.Primero
+	aux := listat.Primero
+	if comprobar.Siguiente != nil && aux != nil {
+		i := listat.Primero
+		for i != nil {
+			j := i.Siguiente
+			for j != nil {
+				if i.Persona.Carnet > j.Persona.Carnet {
+					tmp := i.Persona
+					i.Persona = j.Persona
+					j.Persona = tmp
+				}
+				j = j.Siguiente
+			}
+			i = i.Siguiente
+		}
+	}
 }
